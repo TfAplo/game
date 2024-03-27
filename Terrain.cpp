fc769c759652d94dd2db0982d1b0d93fc7f6b5ab
Tom <tomfouquet1@gmail.com>
2024-03-27 19:35:00 +0100

    suppression de obstacle sol et terrain et ajout de modules

--- Terrain.cpp
+++
@@ -1,5 +0,0 @@
-#include "Terrain.h"
-
-
-Terrain::Terrain(const QString &texture, int width, int height, int posX, int posY)
-    : texture(texture) ,width(width), height(height), posX(posX), posY(posY) {}
